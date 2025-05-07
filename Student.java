121674060d70c8685284b488c1d8ba635ae3f7de
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-07 10:41:03 +0530

    saved

--- Student.java
+++ Student.java
@@ -8,13 +8,35 @@ public class Student {
         this.age = age;
     }
 
-    void show(){
+    void show1(){
         System.out.println("Student Name: " + name + "\nAge: " + age);
     }
 
     public static void main(String[] args) {
         Student s = new Student("rohit", 21);
-        s.show();
+        s.show1();
+        Student_course sc = new Student_course(5, new String[]{"Java", "Python", "C++"}, new int[]{90, 85, 88});
+        sc.show2();
     }
+}
+
+class Student_course {
+    int sem;
+    String[] courseName = new String[3];
+    int[] Marks = new int[3];
+
+    Student_course(int sem, String[] courseName, int[] Marks) {
+        this.sem = sem;
+        this.courseName = courseName;
+        this.Marks = Marks;
+    }
+
+    void show2() {
+        System.out.println("Semester: " + sem);
+        for (int i = 0; i < courseName.length; i++) {
+            System.out.println("Course Name: " + courseName[i] + "\tMarks: " + Marks[i]);
+        }
+    }
+    
 }
 
